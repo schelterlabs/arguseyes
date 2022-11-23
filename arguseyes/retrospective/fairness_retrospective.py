0fede404d2caf9ce766fd2b828e3a4d075909f72
sscdotopen <ssc.open@googlemail.com>
2022-11-23 18:55:28 +0100

    polishing

--- arguseyes/retrospective/fairness_retrospective.py
+++ arguseyes/retrospective/fairness_retrospective.py
@@ -2,6 +2,7 @@ import mlflow
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from IPython.display import display, HTML
 
 
 class FairnessRetrospective:
@@ -17,6 +18,76 @@ class FairnessRetrospective:
                 criterion = (key.split('.')[2], key.split('.')[3])
                 criteria.add(criterion)
         return criteria
+
+    def show_confusion_matrix(self, sensitive_attribute, privileged_class):
+
+        def from_counts(counts, sensitive_attribute, privileged_class, score):
+            the_slice = counts
+            the_slice = the_slice[the_slice.sensitive_attribute == sensitive_attribute]
+            the_slice = the_slice[the_slice.privileged_class == privileged_class]
+            the_slice = the_slice[the_slice.sensitive_attribute == sensitive_attribute]
+            the_slice = the_slice[the_slice.score == score]
+            return int(list(the_slice.count_privileged)[0]), int(list(the_slice.count_disadvantaged)[0])
+
+        counts = self.confusion_matrices_for_groups()
+
+        tn_priv, tn_dis = from_counts(counts, sensitive_attribute, privileged_class, 'true_negatives')
+        fp_priv, fp_dis = from_counts(counts, sensitive_attribute, privileged_class, 'false_positives')
+        fn_priv, fn_dis = from_counts(counts, sensitive_attribute, privileged_class, 'false_negatives')
+        tp_priv, tp_dis = from_counts(counts, sensitive_attribute, privileged_class, 'true_positives')
+
+        html = f"""
+        <div style="display:flex; margin-left:-5px; margin-right:-5px; width: 85%;">
+          <div style="flex: 50%; padding: 5px;">
+            <h5>Confusion matrix for the privileged group ({sensitive_attribute}={privileged_class})</h5>
+            <table>
+              <tr>
+                <td></td><td><b>positive prediction</b></td><td><b>negative prediction</b></td><td></td>
+              </tr>
+              <tr>
+                <td><b>positive label</b></td>
+                <td style="border-left: 2px solid black;border-top: 2px solid black;">TP={tp_priv}</td>
+                <td style="border-right: 2px solid black;border-top: 2px solid black;">FN={fn_priv}</td>
+                <td>{tp_priv + fn_priv}</td>
+              </tr>    
+              <tr>
+                <td><b>negative label</b></td>
+                <td style="border-left: 2px solid black;border-bottom: 2px solid black;">FP={fp_priv}</td>
+                <td style="border-right: 2px solid black;border-bottom: 2px solid black;">TN={tn_priv}</td>
+                <td>{fp_priv + tn_priv}</td>
+              </tr>  
+              <tr>
+                <td></td><td>{tp_priv + fp_priv}</td><td>{fn_priv + tn_priv}</td><td></td>
+              </tr>    
+            </table>
+          </div>
+          <div style="flex: 50%; padding: 5px;">
+            <h5>Confusion matrix for the disadvantaged group ({sensitive_attribute}!={privileged_class})</h5>  
+            <table>
+              <tr>
+                <td></td><td><b>positive prediction</b></td><td><b>negative prediction</b></td><td></td>
+              </tr>
+              <tr>
+                <td><b>positive label</b></td>
+                <td style="border-left: 2px solid black;border-top: 2px solid black;">TP={tp_dis}</td>
+                <td style="border-right: 2px solid black;border-top: 2px solid black;">FN={fn_dis}</td>
+                <td>{tp_dis + fn_dis}</td>
+              </tr>    
+              <tr>
+                <td><b>negative label</b></td>
+                <td style="border-left: 2px solid black;border-bottom: 2px solid black;">FP={fp_dis}</td>
+                <td style="border-right: 2px solid black;border-bottom: 2px solid black;">TN={tn_dis}</td>
+                <td>{fp_dis + tn_dis}</td>
+              </tr>  
+              <tr>
+                <td></td><td>{tp_dis + fp_dis}</td><td>{fn_dis + tn_dis}</td><td></td>
+              </tr>    
+            </table>
+          </div>  
+        </div>  
+        """
+
+        display(HTML(html))
 
     def confusion_matrices_for_groups(self):
         criteria = self.fairness_criteria()
